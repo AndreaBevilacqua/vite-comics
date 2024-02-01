0f2cb77bf9789e44a2c4569b62f6b10ba4797dd9
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-01 15:08:23 +0100

    added generics css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,92 @@
 <script>
-export default {
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+import AppMain from './components/AppMain.vue'
 
+export default {
+  data: () => ({
+    links: [
+      {
+        text: 'Characters',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Comics',
+        url: '#',
+        current: true,
+      },
+      {
+        text: 'Movies',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'TV',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Games',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Collectibles',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Videos',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Fans',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'News',
+        url: '#',
+        current: false,
+      },
+      {
+        text: 'Shop',
+        url: '#',
+        current: false,
+      },
+    ]
+  }),
+  components: {
+    AppHeader,
+    AppFooter,
+    AppMain
+  }
 }
 </script>
 
 <template></template>
 
-<style></style>
+<style>
+/* Reset */
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* Generics */
+
+body {
+  max-height: 100vh;
+}
+
+/* Utils */
+
+ul li {
+  text-decoration: none;
+  list-style-type: none;
+}
+</style>

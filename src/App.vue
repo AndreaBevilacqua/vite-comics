1f491f86dc1066acd53988b0f7162b4e949a5498
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-02 09:46:57 +0100

    fixed header navbar

--- src/App.vue
+++ src/App.vue
@@ -5,60 +5,6 @@ import AppFooterBot from './components/AppFooterBot.vue';
 import AppMain from './components/AppMain.vue';
 
 export default {
-  data: () => ({
-    links: [
-      {
-        text: 'Characters',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Comics',
-        url: '#',
-        current: true,
-      },
-      {
-        text: 'Movies',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'TV',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Games',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Collectibles',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Videos',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Fans',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'News',
-        url: '#',
-        current: false,
-      },
-      {
-        text: 'Shop',
-        url: '#',
-        current: false,
-      },
-    ]
-  }),
   components: {
     AppHeader,
     AppFooterTop,
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,60 @@
 <script>
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    data: () => ({
+        links: [
+            {
+                text: 'Characters',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Comics',
+                url: '#',
+                current: true,
+            },
+            {
+                text: 'Movies',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'TV',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Games',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Collectibles',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Videos',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Fans',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'News',
+                url: '#',
+                current: false,
+            },
+            {
+                text: 'Shop',
+                url: '#',
+                current: false,
+            },
+        ]
+    }),
 }
 </script>
 
@@ -10,11 +64,7 @@ export default {
             <img src="../assets/img/dc-logo.png" alt="dc-logo">
             <div id="navbar">
                 <ul>
-                    <li class="active"><a href="#"></a>POLLO</li>
-                    <li><a href="#"></a>POLLO</li>
-                    <li><a href="#"></a>POLLO</li>
-                    <li><a href="#"></a>POLLO</li>
-                    <li><a href="#"></a>POLLO</li>
+                    <li v-for="link in links"><a href="#"></a>{{ link.text }}</li>
                 </ul>
             </div>
         </section>
